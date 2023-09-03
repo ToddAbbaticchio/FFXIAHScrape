5a6fd4cf685dcd8aa7c1e4f786de9f4773bb9a85
ToddAbbaticchio <102173669+ToddAbbaticchio@users.noreply.github.com>
2023-09-03 13:58:25 -0400

    Update Constants.cs

--- FFXIAHScrape/Constants.cs
+++ FFXIAHScrape/Constants.cs
@@ -7,7 +7,7 @@ namespace FFXIAHScrape
         public static string baseUrl = "https://www.ffxiah.com/item";
         public static string stack = "?stack=1";
         
-        public const string discordRareItemWebhook = "https://discord.com/api/webhooks/1038608360729227394/czT4x2_GfkTPnLx9c0pVCULYiwjJQ38AhzcEJlN6YdQPIwRDwZF47NgJJhkhuzNooGMa";
-        public const ulong discordRareItemMessageId = 1043921036690870322;
+        //public const string discordRareItemWebhook = "redacted";
+        //public const ulong discordRareItemMessageId = redacted;
     }
 }
